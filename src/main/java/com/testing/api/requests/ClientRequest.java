d9d3704c42d51411e5b479f53908dc8f1dcd6c3f
helen-bernal <helen.bernal@globant.com>
2024-11-29 16:01:23 -0500

    initial project

---
+++ src/main/java/com/testing/api/requests/ClientRequest.java
@@ -0,0 +1,101 @@
+package com.testing.api.requests;
+
+import com.google.gson.Gson;
+import com.testing.api.models.Client;
+import com.testing.api.utils.Constants;
+import com.testing.api.utils.JsonFileReader;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ClientRequest extends BaseRequest {
+    
+    private String endpoint;
+    
+    /**
+     Get Client list
+     
+     @return rest-assured response
+     */
+    public Response getClients() {
+        endpoint = String.format(Constants.URL, Constants.CLIENTS_PATH);
+        return requestGet(endpoint, createBaseHeaders());
+    }
+    
+    /**
+     Get client by id
+     
+     @param clientId string
+     @return rest-assured response
+     */
+    public Response getClient(String clientId) {
+        endpoint = String.format(Constants.URL_WITH_PARAM, Constants.CLIENTS_PATH, clientId);
+        return requestGet(endpoint, createBaseHeaders());
+    }
+    
+    /**
+     Update client by id
+     
+     @param client model
+     @param clientId string
+     @return rest-assured response
+     */
+    public Response updateClient(Client client, String clientId) {
+        endpoint = String.format(Constants.URL_WITH_PARAM, Constants.CLIENTS_PATH, clientId);
+        return requestPut(endpoint, createBaseHeaders(), client);
+    }
+    
+    /**
+     Delete client by id
+     
+     @param clientId string
+     @return rest-assured response
+     */
+    public Response deleteClient(String clientId) {
+        endpoint = String.format(Constants.URL_WITH_PARAM, Constants.CLIENTS_PATH, clientId);
+        return requestDelete(endpoint, createBaseHeaders());
+    }
+    
+    public Client getClientEntity(@NotNull Response response) {
+        return response.as(Client.class);
+    }
+    
+    public List<Client> getClientsEntity(@NotNull Response response) {
+        JsonPath jsonPath = response.jsonPath();
+        return jsonPath.getList("", Client.class);
+    }
+    
+    public Response createDefaultClient() {
+        JsonFileReader jsonFile = new JsonFileReader();
+        return this.createClient(jsonFile.getClientByJson(Constants.DEFAULT_CLIENT_FILE_PATH));
+    }
+    
+    /**
+     Create client
+     
+     @param client model
+     @return rest-assured response
+     */
+    public Response createClient(Client client) {
+        endpoint = String.format(Constants.URL, Constants.CLIENTS_PATH);
+        return requestPost(endpoint, createBaseHeaders(), client);
+    }
+    
+    public Client getClientEntity(String clientJson) {
+        Gson gson = new Gson();
+        return gson.fromJson(clientJson, Client.class);
+    }
+    
+    public boolean validateSchema(Response response, String schemaPath) {
+        try {
+            response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(schemaPath));
+            return true;
+        }
+        catch(AssertionError e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/testing/api/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.testing.api.utils;
+
+public final class Constants {
+    public static final  String VALUE_CONTENT_TYPE       = "application/json";
+    public static final  String CONTENT_TYPE             = "Content-Type";
+    public static final  String CLIENTS_PATH             = "clients";
+    public static final  String DEFAULT_CLIENT_FILE_PATH = "src/main/resources/data/defaultClient.json";
+    public static final String BASE_URL                 = "https://673bdac096b8dcd5f3f7afdb.mockapi.io/api/v1/clients";
+    public static final  String URL                      = "/api/v1/%s";
+    public static final  String URL_WITH_PARAM           = "/api/v1/%s/%s";
+
+    private Constants() {
+    }
+}
